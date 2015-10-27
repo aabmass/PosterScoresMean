f59412d0155d8b91314bf1423644016d1ffc77f3
Aaron Abbott <aabmass@gmail.com>
2015-10-27 18:37:47 -0400

    Got rid of boilerplate 'things'

--- client/app/main.index/main.index.controller.spec.js
+++ client/app/main.index/main.index.controller.spec.js
@@ -2,19 +2,16 @@
 
 describe('Controller: MainIndexCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('posterScoresMeanApp'));
+    // load the controller's module
+    beforeEach(module('posterScoresMeanApp'));
 
-  var MainIndexCtrl, scope;
+    var MainIndexCtrl, scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainIndexCtrl = $controller('MainIndexCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should ...', function () {
-  });
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainIndexCtrl = $controller('MainIndexCtrl', {
+            $scope: scope
+        });
+    }));
 });
